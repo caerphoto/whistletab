1e0343b4752ad63caae3015cfebed12d98f5a41c
Andy Farrell <andrew.farrell@experian.com>
2018-04-11 20:32:19 +0100

    Add logging and fix paths for dev

--- server.js
+++ server.js
@@ -23,27 +23,44 @@ app.set('trust proxy', true);
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
+function log(eventString, request) {
+  console.log([
+    new Date(),
+    '[' + eventString + ']',
+    request.method,
+    request.path,
+    request.ip,
+    request.url
+  ].join(' '));
+}
+
 app.get('/whistletab', function (req, res) {
   var serverStore = false;
   if (req.query.store === 'server') {
     serverStore = true;
   }
   res.render('index.ejs', { serverStore: serverStore });
+
+  log('', req);
 });
 
 app.get('/whistletab/tabs', function (req, res) {
   fs.readFile('tabs.json', 'utf8', function (err, jsonString) {
     var tabs;
+    var logInfo = 'read and parse';
 
     if (err) {
+      log('file open error', req);
       return res.status(500).end();
     }
     try {
       tabs = JSON.parse(jsonString);
     } catch (parseError) {
       tabs = DEFAULT_TABS;
+      logInfo = 'parse failed';
     }
     res.json(tabs);
+    log(logInfo, req);
   });
 });
 
@@ -53,13 +70,16 @@ app.post('/whistletab/tabs', function (req, res) {
   try {
     jsonString = JSON.stringify(req.body);
   } catch (stringifyError) {
+    log('unable to parse sent data', req);
     return res.status(400).end();
   }
 
   fs.writeFile('tabs.json', jsonString, 'utf8', function (err) {
     if (err) {
+      log('file open error', req);
       return res.status(500).end();
     }
+    log('saved new tabs', req);
     res.status(204).end();
   });
 });
